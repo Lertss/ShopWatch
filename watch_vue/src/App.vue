9d0f74dbf925c997a145c9ba9cd3f108519f3cda
Lerts <sergzza0@gmail.com>
2022-11-29 01:16:45 +0100

    Two applications have been created - django and vue. In django, the key is hidden, packages and urls are added. In vue, the start of creating the layout of the main page.

---
+++ watch_vue/src/App.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Watch</strong></router-link>
+
+          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+       </div>
+
+        <div class="navbar-menu" id="navbar-menu">
+          <div class="navbar-end">
+            <router-link to="/mens" class="navbar-item">Men's watches</router-link>
+            <router-link to="/womens" class="navbar-item">Women's watches.</router-link>
+
+            <div class="navbar-item">
+              <div class="buttons">
+                <router-link to="/log-in" class="button is-light">Log in</router-link>
+
+                <router-link to="/cart" class="button is-success ">
+                  <span class="icon"><el-icon><ShoppingCartFull /></el-icon></span>
+                  <span>Cart</span>
+                </router-link>
+
+              </div>
+            </div>
+          </div>
+      </div>
+    </nav>
+         <router-view/>
+  </div>
+
+</template>
+
+
+<style lang="scss">
+@import "../node_modules/bulma";
+
+</style>
+<script setup>
+import {ShoppingCartFull} from '@element-plus/icons-vue'
+</script>
+
+
