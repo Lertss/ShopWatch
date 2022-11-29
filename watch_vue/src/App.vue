8a7b04cc6f2bb4c0417096428fc1b1b97ad97914
Lerts <sergzza0@gmail.com>
2022-11-29 01:43:33 +0100

    Added footer and drop-down menus for the phone.

--- watch_vue/src/App.vue
+++ watch_vue/src/App.vue
@@ -1,18 +1,18 @@
 
-<template>
+<template xmlns="http://www.w3.org/1999/html">
   <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Watch</strong></router-link>
 
-          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = ! showMobileMenu">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
        </div>
 
-        <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
           <div class="navbar-end">
             <router-link to="/mens" class="navbar-item">Men's watches</router-link>
             <router-link to="/womens" class="navbar-item">Women's watches.</router-link>
@@ -31,7 +31,12 @@
           </div>
       </div>
     </nav>
-         <router-view/>
+    <section class="section">
+       <router-view/>
+    </section>
+        <footer class="footer">
+          <p class="has-text-centered">Copyright (c) 2022</p>
+        </footer>
   </div>
 
 </template>
@@ -39,10 +44,19 @@
 
 <style lang="scss">
 @import "../node_modules/bulma";
-
 </style>
+
 <script setup>
 import {ShoppingCartFull} from '@element-plus/icons-vue'
+
 </script>
 
-
+<script>
+export default  {
+  data() {
+    return {
+      showMobileMenu:false,
+    }
+  }
+}
+</script>
