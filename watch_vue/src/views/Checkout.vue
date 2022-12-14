baa4d61ef41a6ab64088eebd58b15b0d03cc3f5b
Serhii Nazarov <93787712+Lertss@users.noreply.github.com>
2022-12-14 02:28:08 +0100

    Added url, registered order and OrderItem models in the admin panel. Created serializers in the orders application. Created the Order function through decorators, and the OrderList class in view.py.

--- watch_vue/src/views/Checkout.vue
+++ watch_vue/src/views/Checkout.vue
@@ -141,7 +141,7 @@ export default {
     document.title = 'Checkout | Watch'
     this.cart = this.$store.state.cart
     if (this.cartTotalLength > 0) {
-      this.stripe = Stripe('pk_test_51H1HiuKBJV2qfWbD2gQe6aqanfw6Eyul5PO2KeOuSRlUMuaV4TxEtaQyzr9DbLITSZweL7XjK3p74swcGYrE2qEX00Hz7GmhMI')
+      this.stripe = Stripe('secret_key')
       const elements = this.stripe.elements();
       this.card = elements.create('card', { hidePostalCode: true })
       this.card.mount('#card-element')
